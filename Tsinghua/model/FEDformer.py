7bbdde86266c9944db1f08ae94a6b6125c699c09
LongLee220 <78303456+LongLee220@users.noreply.github.com>
2025-06-27 12:57:17 +0800

    Update FEDformer.py

--- Tsinghua/model/FEDformer.py
+++ Tsinghua/model/FEDformer.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from layers.Transformer_EncDec import Encoder, EncoderLayer
+from layers.Transformer_EncDec_fed import Encoder, EncoderLayer
 from layers.Autoformer_EncDec import my_Layernorm
 from layers.AutoCorrelation import AutoCorrelationLayer
 from layers.FourierCorrelation import FourierBlock
